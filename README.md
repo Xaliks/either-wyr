7cdeaaae5d9f0f4cb648964c8ccc4684d77f9061
Xaliks <miskinmatvej77@gmail.com>
2023-01-20 14:32:44 +0300

    format & fix

--- README.md
+++ README.md
@@ -26,19 +26,19 @@ WouldYouRather().then(console.log); // [Question]
   commentCount: Number,
   createdAt: Date,
   updatedAt: Date,
-	totalVotes: Number,
+  totalVotes: Number,
   options: [
     {
       type: "blue",
       question: String,
       voteCount: Number,
-			percentage: String,
+      percentage: String,
     },
     {
       type: "red",
       question: String,
       voteCount: Number,
-			percentage: String,
+      percentage: String,
     }
   ],
   author: {
@@ -60,53 +60,53 @@ WouldYouRather().then(console.log); // [Question]
 }
 ```
 
-#### Example reponse
+#### Example response
 ```json
 [
-	{
-		"id": 123,
-		"isAdult": false,
-		"title": "LOL",
-		"description": null,
-		"sentence": "Would you rather be very fat or be very thin?",
-		"url": "http://either.io/123/fat-thin",
-		"slug": "fat-thin",
-		"shortUrl": "http://wyr.be/bG1hbw",
-		"commentCount": 1337,
-		"createdAt": "2009-10-08T20:22:43.000Z",
-		"updatedAt": "2009-10-08T22:05:30.000Z",
-		"totalVotes": 246912,
-		"options": [
-			{
-				"type": "blue",
-				"question": "Be very fat",
-				"voteCount": 123456,
-				"percentage": "50.00"
-			},
-			{
-				"type": "red",
-				"question": "Be very thin",
-				"voteCount": 123456,
-				"percentage": "50.00"
-			}
-		],
-		"author": {
-			"id": 123987,
+  {
+    "id": 123,
+    "isAdult": false,
+    "title": "LOL",
+    "description": null,
+    "sentence": "Would you rather be very fat or be very thin?",
+    "url": "http://either.io/123/fat-thin",
+    "slug": "fat-thin",
+    "shortUrl": "http://wyr.be/bG1hbw",
+    "commentCount": 1337,
+    "createdAt": "2009-10-08T20:22:43.000Z",
+    "updatedAt": "2009-10-08T22:05:30.000Z",
+    "totalVotes": 246912,
+    "options": [
+      {
+        "type": "blue",
+        "question": "Be very fat",
+        "voteCount": 123456,
+        "percentage": "50.00"
+      },
+      {
+        "type": "red",
+        "question": "Be very thin",
+        "voteCount": 123456,
+        "percentage": "50.00"
+      }
+    ],
+    "author": {
+      "id": 123987,
       "isDeleted": false,
-			"name": "Xaliks",
-			"url": "http://either.io/user/123987",
-			"email": "email@lol.com"
-		},
-		"tags": [
-			{
-				"id": 8,
-				"name": "Sex",
-				"url": "http://either.io/tags/11/sex",
-				"slug": "sex",
-				"createdAt": "2012-01-10T16:22:33.000Z"
-			}
-		]
-	}
+      "name": "Xaliks",
+      "url": "http://either.io/user/123987",
+      "email": "email@lol.com"
+    },
+    "tags": [
+      {
+        "id": 8,
+        "name": "Sex",
+        "url": "http://either.io/tags/11/sex",
+        "slug": "sex",
+        "createdAt": "2012-01-10T16:22:33.000Z"
+      }
+    ]
+  }
 ]
 
 ```
--- index.js
+++ index.js
@@ -1,56 +1,67 @@
 const { fetch } = require("undici");
 
 async function getQuestions(options = { count: 1 }) {
-  if (typeof options.count !== "number") throw new Error("either-wyr", `[options.count] Must be number! Provided: ${typeof count}`);
+	if (typeof options.count !== "number") {
+		throw new Error("either-wyr", `[options.count] Must be number! Provided: ${typeof options.count}`);
+	}
+	if (options.count <= 0) {
+		throw new Error("either-wyr", `[options.count] Must be more than 0! Provided: ${options.count}`);
+	}
 
-  const questions = await fetch(`http://either.io/questions/next/${options.count}`).then((response) => response.json());
+	const questions = await fetch(`http://either.io/questions/next/${options.count}`).then((response) => response.json());
 
-  return questions.questions.map((question) => {
-    return {
-      id: parseInt(question.id),
-      isAdult: question.is_adult !== "0",
-      title: question.title,
-      description: question.moreinfo || null,
-      sentence: question.twitter_sentence,
-      url: `http://either.io/${question.id}/${question.slug}`,
-      slug: question.slug,
-      shortUrl: question.short_url,
-      commentCount: parseInt(question.comment_total) || 0,
-      createdAt: new Date(parseInt(question.created_on) * 1000),
-      updatedAt: new Date(parseInt(question.updated_on) * 1000),
-      totalVotes: parseInt(question.option1_total) + parseInt(question.option2_total),
-      options: [
-        {
-          type: "blue",
-          question: question.option_1,
-          voteCount: parseInt(question.option1_total),
-          percentage: (parseInt(question.option1_total) / (parseInt(question.option1_total) + parseInt(question.option2_total)) * 100).toFixed(2),
-        },
-        {
-          type: "red",
-          question: question.option_2,
-          voteCount: parseInt(question.option2_total),
-          percentage: (parseInt(question.option2_total) / (parseInt(question.option1_total) + parseInt(question.option2_total)) * 100).toFixed(2),
-        },
-      ],
-      author: {
-        id: parseInt(question.user_id),
-        name: question.display_name,
-        isDeleted: question.user_is_deleted !== "0",
-        url: `http://either.io/user/${question.user_id}`,
-        email: question.email || null,
-      },
-      tags: question.tags.map((tag) => {
-        return {
-          id: parseInt(tag.id),
-          name: tag.name.replace(/&amp;amp;/g, "&").replace(/&amp;/g, "&"),
-          url: `http://either.io/tags/${tag.id}/${tag.slug}`,
-          slug: tag.slug,
-          createdAt: new Date(parseInt(tag.created_on) * 1000),
-        }
-      })
-    }
-  })
+	return questions.questions.map((question) => {
+		return {
+			id: parseInt(question.id),
+			isAdult: question.is_adult !== "0",
+			title: question.title,
+			description: question.moreinfo || null,
+			sentence: question.twitter_sentence,
+			url: `http://either.io/${question.id}/${question.slug}`,
+			slug: question.slug,
+			shortUrl: question.short_url,
+			commentCount: parseInt(question.comment_total) || 0,
+			createdAt: new Date(parseInt(question.created_on) * 1000),
+			updatedAt: new Date(parseInt(question.updated_on) * 1000),
+			totalVotes: parseInt(question.option1_total) + parseInt(question.option2_total),
+			options: [
+				{
+					type: "blue",
+					question: question.option_1,
+					voteCount: parseInt(question.option1_total),
+					percentage: (
+						(parseInt(question.option1_total) / (parseInt(question.option1_total) + parseInt(question.option2_total))) *
+						100
+					).toFixed(2),
+				},
+				{
+					type: "red",
+					question: question.option_2,
+					voteCount: parseInt(question.option2_total),
+					percentage: (
+						(parseInt(question.option2_total) / (parseInt(question.option1_total) + parseInt(question.option2_total))) *
+						100
+					).toFixed(2),
+				},
+			],
+			author: {
+				id: parseInt(question.user_id),
+				name: question.display_name,
+				isDeleted: question.user_is_deleted !== "0",
+				url: `http://either.io/user/${question.user_id}`,
+				email: question.email || null,
+			},
+			tags: question.tags.map((tag) => {
+				return {
+					id: parseInt(tag.id),
+					name: tag.name.replace(/&amp;amp;/g, "&").replace(/&amp;/g, "&"),
+					url: `http://either.io/tags/${tag.id}/${tag.slug}`,
+					slug: tag.slug,
+					createdAt: new Date(parseInt(tag.created_on) * 1000),
+				};
+			}),
+		};
+	});
 }
 
 module.exports = getQuestions;
